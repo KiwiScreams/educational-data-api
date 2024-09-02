acdd0d62c81e8101911d1d3f6598ff2e85473b90
KiwiScreams <screamskiwi3@gmail.com>
2024-09-02 11:32:33 +0400

    api request

--- src/app.js
+++ src/app.js
@@ -1,1 +1,7 @@
-console.log("Hello");
+import express from "express";
+const app = express();
+app.get("/", async (_, res) => {
+  const users = [{ name: "Salome" }];
+  return res.status(200).json({ message: "Api Requessts!", users });
+});
+app.listen(3000);
